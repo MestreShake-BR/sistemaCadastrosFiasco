af93930dab68c39cfca02ad34105bc4e6508d568
Alexandre Stálin Silva Alves <aalves@institutomonitor.com.br>
2026-01-16 20:13:04 -0300

    primeiro controller

---
+++ src/main/java/com/cadastrod/cadastro/controller/NewController.java
@@ -0,0 +1,23 @@
+package com.cadastrod.cadastro.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NewController {
+    /*
+    @PostMapping -- MANDAR
+    @PutMapping -- ALTERAR
+    @PatchMapping -- ALTERAR (VERIFICAR A DIFERENÇA DO PUT)
+    @DeleteMapping -- DELETAR
+    @GetMapping -- PUXAR
+    */
+
+    @GetMapping("/first")
+    public String boasVindas(){
+        return "My firsting message";
+    }
+}
